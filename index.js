17f64b821d3b97ab8623591249c3b9a1d7989e0b
Abby Heiser <aheiser2@yahoo.com>
2023-05-24 03:25:30 -0500

    Add delete buttons, add book ids, add read status toggle, add form open and close ability, start css

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+let bookId = 3
+
 let myLibrary = [
     {
         title: "Memoirs of a Geisha",
         author: "Arthur Golden",
         pages: 275,
-        readStatus: "read"
+        readStatus: "read",
+        id: 1
     },
     {
         title: "Frankenstein",
         author: "Mary Shelley",
         pages: 157,
-        readStatus: "Not read"
+        readStatus: "Not read",
+        id: 2
     }
 
 ];
@@ -19,6 +23,7 @@ function Book(title, author, pages, readStatus){
     this.author = author
     this.pages = pages
     this.readStatus = readStatus
+    this.id = bookId++
 }
 
 Book.prototype.info = function() {
@@ -26,26 +31,36 @@ Book.prototype.info = function() {
         " pages, " + this.readStatus
 }
 
+
+function toggleReadStatus(id){
+    var element = document.getElementById(`${id}-readStatus`);
+    console.log(element)
+    element.classList.toggle("mystyle");
+    if(element.innerHTML === "read"){
+        element.innerHTML = "Not read"
+    } else if (element.innerHTML === "Not read"){
+        element.innerHTML = "read"
+    }
+}
+
 const book1 = new Book("LOTR", "JRR Tolkein", 750, "read")
 myLibrary.push(book1)
 
-// console.log(book1.info())
-
-// let text = ""
-// document.getElementById("myLibrary").innerHTML = text
-
-function showLibrary(myLibrary){
-    return "<div class=\"book-cards\">" + myLibrary.map(book => `
-    <div>
+function showLibrary(myLibrary){ 
+    return myLibrary.map(book => `  
+    <div class="book-cards" id="${book.id}">
       <div class="book-header">
         <h3>${book.title}</h3>
       </div>
       <div class="book-info">
         <h4>${book.author}</h4>
         <p>${book.pages}</p>
+        <p id="${book.id}-readStatus" onclick="toggleReadStatus(${book.id})">${book.readStatus}</p>
+       <p>${book.id}</p>
       </div> 
-      <button class="info-button">+ info</button>
-    </div>`).join('') + "</div>";
+      <button class="delete-button" onclick="deleteBook(${book.id})">Delete</button>
+    </div>`)
+    .join('');
 }
 
 document.getElementById("myLibrary").innerHTML = showLibrary(myLibrary)
@@ -56,7 +71,7 @@ function addBookToLibrary() {
     let pages = document.getElementById("pages").value
     let readStatus = document.getElementById("readStatus").value
     const book = new Book(title, author, pages, readStatus)
-    myLibrary.push(book)
+    myLibrary.unshift(book)
   console.log(book)
   document.getElementById("title").value = ""
   document.getElementById("author").value = ""
@@ -64,3 +79,22 @@ function addBookToLibrary() {
   document.getElementById("readStatus").value = "read"
   document.getElementById("myLibrary").innerHTML = showLibrary(myLibrary)
 }
+
+function deleteBook(id){
+    let toDelete = myLibrary.findIndex(x => x.id === id)
+    console.log(id)
+    console.log(toDelete)
+    document.getElementById(`${id}`).remove()
+    myLibrary.splice(toDelete, 1)
+    document.getElementById("myLibrary").innerHTML = showLibrary(myLibrary)
+
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    addBookToLibrary()
+    document.getElementById("myForm").style.display = "none";
+  }
