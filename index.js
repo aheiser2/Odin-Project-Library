fe725d4054f067a62f081421583c4b77eca14350
Abby Heiser <aheiser2@yahoo.com>
2023-05-27 03:15:36 -0500

    Create book shapes, adjust font-sizing and alignment, create randomizer functions for font/wwriting color/book cover color, add background to website

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 let bookId = 3
+let fonts = ["'Abril Fatface', cursive", "'Castoro Titling', cursive", "'Gabriela', serif", "'Playball', cursive"]
+let bookColors = ["darkblue", "teal", "purple", "crimson", "cornflowerblue", "darkolivegreen", "firebrick",
+    "darkgreen", "darkslateblue", "goldenrod", "lavender", "lightcoral", "lightsteelblue", "mediumorchid", "thistle"]
+
+function writingColor(bookColor){
+    let writingColor = ""
+    if(bookColor == "darkblue" || bookColor == "purple" || bookColor == "crimson" || bookColor == "darkolivvegreen" ||
+    bookColor == "firebrick" || bookColor == "darkgreen" || bookColor == "darkslateblue" || bookColor == "goldenrod"
+    || bookColor == "cornflowerblue"){
+        writingColor = "white"
+    } else {
+        writingColor = "darkblue"
+    }
+    return writingColor
+}
+
+function randomBookColor(){
+    let color = Math.floor(Math.random() * 14);
+    let bookColor = bookColors[color]
+    return bookColor
+}
+
+function randomFont(){
+    let font = fonts[Math.floor(Math.random() * 3)];
+    return font
+}
 
 let myLibrary = [
     {
         title: "Memoirs of a Geisha",
         author: "Arthur Golden",
         pages: 275,
-        readStatus: "read",
-        id: 1
+        readStatus: "Read",
+        id: 1,
+        bookColor: "pink",
+        font: "'Times New Roman', Times, serif"
     },
     {
         title: "Frankenstein",
         author: "Mary Shelley",
         pages: 157,
         readStatus: "Not read",
-        id: 2
+        id: 2,
+        bookColor: "red",
+        font: "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif"
     }
 
 ];
@@ -24,6 +54,9 @@ function Book(title, author, pages, readStatus){
     this.pages = pages
     this.readStatus = readStatus
     this.id = bookId++
+    this.bookColor = randomBookColor()
+    this.writingColor = writingColor(this.bookColor)
+    this.font = randomFont()
 }
 
 Book.prototype.info = function() {
@@ -39,26 +72,25 @@ function toggleReadStatus(id){
     if(element.innerHTML === "read"){
         element.innerHTML = "Not read"
     } else if (element.innerHTML === "Not read"){
-        element.innerHTML = "read"
+        element.innerHTML = "Read"
     }
 }
 
-const book1 = new Book("LOTR", "JRR Tolkein", 750, "read")
+const book1 = new Book("LOTR", "JRR Tolkein", 750, "Read")
 myLibrary.push(book1)
 
 function showLibrary(myLibrary){ 
     return myLibrary.map(book => `  
-    <div class="book-cards" id="${book.id}">
-      <div class="book-header">
+    <div class="book-cards" style="background: ${book.bookColor}; color: ${book.writingColor}" id="${book.id}">
+      <div class="book-header" style="font-family: ${book.font}">
         <h3>${book.title}</h3>
       </div>
       <div class="book-info">
         <h4>${book.author}</h4>
-        <p>${book.pages}</p>
+        <p>Pages: ${book.pages}</p>
         <p id="${book.id}-readStatus" onclick="toggleReadStatus(${book.id})">${book.readStatus}</p>
-       <p>${book.id}</p>
       </div> 
-      <button class="delete-button" onclick="deleteBook(${book.id})">Delete</button>
+      <button class="delete-button" role="button" onclick="deleteBook(${book.id})">Delete</button>
     </div>`)
     .join('');
 }
@@ -70,7 +102,9 @@ function addBookToLibrary() {
     let author = document.getElementById("author").value
     let pages = document.getElementById("pages").value
     let readStatus = document.getElementById("readStatus").value
-    const book = new Book(title, author, pages, readStatus)
+    let bookColor = randomBookColor()
+    const book = new Book(title, author, pages, readStatus, bookColor)
+    // document.getElementById(`${book.id}`).style.backgroundColor = bookColor
     myLibrary.unshift(book)
   console.log(book)
   document.getElementById("title").value = ""
@@ -98,3 +132,6 @@ function openForm() {
     addBookToLibrary()
     document.getElementById("myForm").style.display = "none";
   }
+
+
+  
