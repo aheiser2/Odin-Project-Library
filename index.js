3e3e5a6974dd167d815ec2c0da38945e33109f91
Abby Heiser <aheiser2@yahoo.com>
2023-05-29 19:09:31 -0500

    Finish css formatting for form, buttons, and fonts; add check for proper value input; edit random functions to be based on array length instead of hardcoded number; finalize colors for covers + writing

--- index.js
+++ index.js
@@ -1,53 +1,37 @@
-let bookId = 3
-let fonts = ["'Abril Fatface', cursive", "'Castoro Titling', cursive", "'Gabriela', serif", "'Playball', cursive"]
+let bookId = 1
+let fonts = ["'Abril Fatface', cursive", "'Castoro Titling', cursive", "'Gabriela', serif", "'Playball', cursive",
+    "'Courier New', Courier, monospace", "Georgia, 'Times New Roman', Times, serif", "Arial, Helvetica, sans-serif"]
 let bookColors = ["darkblue", "teal", "purple", "crimson", "cornflowerblue", "darkolivegreen", "firebrick",
-    "darkgreen", "darkslateblue", "goldenrod", "lavender", "lightcoral", "lightsteelblue", "mediumorchid", "thistle"]
+    "darkgreen", "darkslateblue", "goldenrod", "palevioletred", "lightcoral", "lightsteelblue", "mediumorchid", "thistle"]
+let myLibrary = [];
 
-function writingColor(bookColor){
-    let writingColor = ""
-    if(bookColor == "darkblue" || bookColor == "purple" || bookColor == "crimson" || bookColor == "darkolivvegreen" ||
-    bookColor == "firebrick" || bookColor == "darkgreen" || bookColor == "darkslateblue" || bookColor == "goldenrod"
-    || bookColor == "cornflowerblue"){
-        writingColor = "white"
-    } else {
-        writingColor = "darkblue"
-    }
-    return writingColor
-}
-
+//Creates a random book cover color
 function randomBookColor(){
-    let color = Math.floor(Math.random() * 14);
+    let color = Math.floor(Math.random() * bookColors.length); 
     let bookColor = bookColors[color]
     return bookColor
 }
 
+//Creates book cover writing color based on cover's color
+function writingColor(bookColor){
+    let writingColor = ""
+    if(bookColor == "darkblue" || bookColor == "teal" || bookColor == "purple" || bookColor == "crimson" 
+    || bookColor == "cornflowerblue" || bookColor == "darkolivvegreen" || bookColor == "firebrick" 
+    || bookColor == "darkgreen" || bookColor == "darkslateblue" || bookColor == "lightcoral" || bookColor == "mediumorchid"){
+        writingColor = "white"
+    } else {
+        writingColor = "navy"
+    }
+    return writingColor
+}
+
+//Picks a random font for the cover's writing
 function randomFont(){
-    let font = fonts[Math.floor(Math.random() * 3)];
+    let font = fonts[Math.floor(Math.random() * fonts.length)];
     return font
 }
 
-let myLibrary = [
-    {
-        title: "Memoirs of a Geisha",
-        author: "Arthur Golden",
-        pages: 275,
-        readStatus: "Read",
-        id: 1,
-        bookColor: "pink",
-        font: "'Times New Roman', Times, serif"
-    },
-    {
-        title: "Frankenstein",
-        author: "Mary Shelley",
-        pages: 157,
-        readStatus: "Not read",
-        id: 2,
-        bookColor: "red",
-        font: "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif"
-    }
-
-];
-
+//Book constructor
 function Book(title, author, pages, readStatus){
     this.title = title
     this.author = author
@@ -59,26 +43,24 @@ function Book(title, author, pages, readStatus){
     this.font = randomFont()
 }
 
-Book.prototype.info = function() {
+Book.prototype.info = function(){
     return this.title + " by " + this.author + ", " + this.pages + 
         " pages, " + this.readStatus
 }
 
-
+//Toggles read status of book when clicking on it
 function toggleReadStatus(id){
     var element = document.getElementById(`${id}-readStatus`);
     console.log(element)
     element.classList.toggle("mystyle");
-    if(element.innerHTML === "read"){
-        element.innerHTML = "Not read"
-    } else if (element.innerHTML === "Not read"){
+    if(element.innerHTML === "Read"){
+        element.innerHTML = "Not Read Yet"
+    } else if (element.innerHTML === "Not Read Yet"){
         element.innerHTML = "Read"
     }
 }
 
-const book1 = new Book("LOTR", "JRR Tolkein", 750, "Read")
-myLibrary.push(book1)
-
+//Shows all the books in the library on cards
 function showLibrary(myLibrary){ 
     return myLibrary.map(book => `  
     <div class="book-cards" style="background: ${book.bookColor}; color: ${book.writingColor}" id="${book.id}">
@@ -95,16 +77,29 @@ function showLibrary(myLibrary){
     .join('');
 }
 
-document.getElementById("myLibrary").innerHTML = showLibrary(myLibrary)
-
+//Takes in input to add a new book to the library, and generates a cover for it
 function addBookToLibrary() {
     let title = document.getElementById("title").value
     let author = document.getElementById("author").value
     let pages = document.getElementById("pages").value
     let readStatus = document.getElementById("readStatus").value
+    if(readStatus == "read"){
+        readStatus = "Read"
+    } else {
+        readStatus = "Not Read Yet"
+    }
+    if (title == ""){
+        alert("Please fill out book title")
+        return
+    } else if (author == ""){
+        alert("Please fill out author")
+        return
+    } else if(!/^\d+$/.test(pages)){
+        alert("Please only enter numbers")
+        return
+    }
     let bookColor = randomBookColor()
-    const book = new Book(title, author, pages, readStatus, bookColor)
-    // document.getElementById(`${book.id}`).style.backgroundColor = bookColor
+    const book = new Book(title, author, pages, readStatus)
     myLibrary.unshift(book)
   console.log(book)
   document.getElementById("title").value = ""
@@ -114,6 +109,7 @@ function addBookToLibrary() {
   document.getElementById("myLibrary").innerHTML = showLibrary(myLibrary)
 }
 
+//Removes a book from the library 
 function deleteBook(id){
     let toDelete = myLibrary.findIndex(x => x.id === id)
     console.log(id)
@@ -124,14 +120,15 @@ function deleteBook(id){
 
 }
 
+//Functions to open and close the "New Book" form
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
+}
+
+function closeForm() {
     addBookToLibrary()
     document.getElementById("myForm").style.display = "none";
-  }
+}
 
 
   
